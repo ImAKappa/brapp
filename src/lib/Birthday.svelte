ee3980989ff789289292786bfed8bf2b442728d0
ImAKappa <imaninconsp1cuouskappa@gmail.com>
2022-06-05 18:21:56 -0400

    Init svelte-vite template

---
+++ src/lib/Birthday.svelte
@@ -0,0 +1,15 @@
+<script>
+    export let name = 'Jesus Christ';
+    export let date = new Date();
+</script>
+
+<div class="birthday">
+    <p>Name: {name}</p>
+    <p>Date: {date}</p>
+</div>
+
+<style>
+    .birthday {
+        border: 1px solid black;
+    }
+</style>
